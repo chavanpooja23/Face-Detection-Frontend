59ab070e9e5e3f1c48a1ecd0e605098dd91df28a
Pooja Chavan <142746336+chavanpooja23@users.noreply.github.com>
2024-01-19 15:13:20 -0500

    final URL change

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://face-detection-api-itfn.onrender.com', {
+    fetch('https://face-detection-api-itfn.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://face-detection-api-itfn.onrender.com', {
+    fetch('https://face-detection-api-itfn.onrender.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
